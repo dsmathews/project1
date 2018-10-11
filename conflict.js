2671cbbe76d84f25672ccd9f07aa9e19e977cb28
Darrell Mathews <smathewsbd@gmail.com>
2018-10-11 10:38:47 -0400

    conflict1

---
+++ conflict.js
@@ -0,0 +1,1 @@
+console.log ("This is a test. It is only a test of the emergency conflict system. In the event of an actual emergency, please place head on wall and the beging to band head on wall.");
