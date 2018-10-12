f800b8372332f8a9e43510b3dd1967ee49b23989
Makiko Vaughan <makikovaughan@Makikos-MacBook-Pro.local>
2018-10-12 10:30:40 -0400

    Updated the face js file

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-const image = "https://res.cloudinary.com/dyais46lc/image/upload/v1539213797/40648536_10156056664519285_7445547226065010688_n.jpg"
+
+// var cloudinary = require('cloudinary');
+
+// cloudinary.config({ 
+//     cloud_name: 'dyais46lc', 
+//     api_key: '169225284978189', 
+//     api_secret: 'TkVw1kJXn8OeNow9f10qSSA8VS4' 
+//   });
+  
+//   cloudinary.imageTag('40648536_10156056664519285_7445547226065010688_n.jpg', {height: 539, radius: 8, width: 613, crop: "scale"}).toHtml();
+
+const image = "https://res.cloudinary.com/dyais46lc/image/upload/v1539310622/faceSquare.jpg"
 
 let faceResult = {
     height:"",
@@ -19,4 +30,10 @@ let faceResult = {
 
 requestFace(image, function (faceResult) {
     console.log(`Face information: ${faceResult.age}`);
+
+    // $("img#mv-test").imgAreaSelect({ x1: 207, y1: 207, x2: 446, y2:  339});
+
+    clipFace(faceResult);
+    console.log("test");
+
 });
