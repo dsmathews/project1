f5583c24988f88f8aa807041f8c36f2d56bcde44
Makiko Vaughan <makikovaughan@Makikos-MBP.attlocal.net>
2018-10-13 10:37:24 -0400

    Made it work to upload and clip the picture

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
+$(document).ready(function () {
 
-// const image = "https://res.cloudinary.com/dyais46lc/image/upload/v1539310622/faceSquare.jpg"
+    var faceResult = {
+        height: "",
+        width: "",
+        top: "",
+        left: "",
+        gender: "",
+        age: "",
+        emotion: {},
+        skinstatus: {},
+        headpose: {}
 
-let faceResult = {
-    height:"",
-    width:"",
-    top:"",
-    left:"",
-    gender:"",
-    age:"",
-    emotion:{},
-    skinstatus:{},
-    headpose:{}
+    };
 
-};
+    var myUploadWidget;
 
-getUrl(url, function (faceResult){
+    document.getElementById("upload_widget_opener").addEventListener("click", function () {
+        myUploadWidget = cloudinary.openUploadWidget({
+            cloudName: 'dyais46lc', uploadPreset: 'tqz7drcd'
+        }, (error, result) => {
 
-    var image = url;
-    console.log(image);
+            if (result && result.event === "success") {
+                console.log(result, result.event);
 
-    requestFace(image, cb);
-    clipFace(faceResult);
+                let facePlusUrl = result.info.url;
+                let pictureName = result.info.path;
+
+
+                requestFace(facePlusUrl, function (faceResult) {
+
+
+                    console.log(`Face information: ${faceResult.age}`);
+
+                    clipFace(faceResult, pictureName);
+                    console.log("test");
+
+                });
+            }
+
+        }, false);
 
     });
+    // const image = "https://res.cloudinary.com/dyais46lc/image/upload/v1539310622/faceSquare.jpg"
+
+    // requestFace(facePlusUrl, function (faceResult) {
 
 
+    //     console.log(`Face information: ${faceResult.age}`);
 
-// requestFace(image, function (faceResult) {
-//     console.log(`Face information: ${faceResult.age}`);
+    //     clipFace(faceResult);
+    //     console.log("test");
 
-
-//     clipFace(faceResult);
-//     console.log("test");
-
-// });
-
-$(".submitDeclare").on("click", translate);
+    // });
 
 
+});
--- face.js
+++ face.js
@@ -1,100 +1,105 @@
-let faceInformation = {
-    height:"",
-    width:"",
-    top:"",
-    left:"",
-    gender:"",
-    age:"",
-    emotion:{},
-    skinstatus:{},
-    headpose:{}
+// $(document).ready(function () {
 
-}
+    let faceInformation = {
+        height: "",
+        width: "",
+        top: "",
+        left: "",
+        gender: "",
+        age: "",
+        emotion: {},
+        skinstatus: {},
+        headpose: {}
 
-const analyzeFace = function(apiKey, apiSecret, faceToken, face, cb) {
+    }
 
-    const attributes = `gender,age,emotion,skinstatus,headpose`;
+    const analyzeFace = function (apiKey, apiSecret, faceToken, face, cb) {
 
-    const analysisURL = `https://api-us.faceplusplus.com/facepp/v3/face/analyze?api_key=${apiKey}&api_secret=${apiSecret}&face_tokens=${faceToken}&return_attributes=${attributes}`;
+        const attributes = `gender,age,emotion,skinstatus,headpose`;
 
-    console.log(analysisURL);
+        const analysisURL = `https://api-us.faceplusplus.com/facepp/v3/face/analyze?api_key=${apiKey}&api_secret=${apiSecret}&face_tokens=${faceToken}&return_attributes=${attributes}`;
 
-    $.ajax({
-        url: analysisURL,
-        method: 'POST',
-    }).then(function (response) {
+        console.log(analysisURL);
 
-        console.log(response);
+        $.ajax({
+            url: analysisURL,
+            method: 'POST',
+        }).then(function (response) {
 
-        faceInformation.height = face[0].face_rectangle.height;
-        faceInformation.width = face[0].face_rectangle.width;
-        faceInformation.top = face[0].face_rectangle.top;
-        faceInformation.left = face[0].face_rectangle.left;
+            console.log(response);
 
-        faceInformation.gender = response.faces[0].attributes.gender.value; 
-        faceInformation.age = response.faces[0].attributes.age.value;
-        faceInformation.emotion = response.faces[0].attributes.emotion;
-        faceInformation.skinstatus = response.faces[0].attributes.skinstatus;
-        faceInformation.headpose = response.faces[0].attributes.headpose;
+            faceInformation.height = face[0].face_rectangle.height;
+            faceInformation.width = face[0].face_rectangle.width;
+            faceInformation.top = face[0].face_rectangle.top;
+            faceInformation.left = face[0].face_rectangle.left;
 
-        console.log(`Analyze: ${faceInformation.height}`);
+            faceInformation.gender = response.faces[0].attributes.gender.value;
+            faceInformation.age = response.faces[0].attributes.age.value;
+            faceInformation.emotion = response.faces[0].attributes.emotion;
+            faceInformation.skinstatus = response.faces[0].attributes.skinstatus;
+            faceInformation.headpose = response.faces[0].attributes.headpose;
 
-        cb(faceInformation);
+            console.log(`Analyze: ${faceInformation.height}`);
 
-    }).catch(function () {
-        alert("Could not retrieve the information. Please try again later.")
-    });
+            cb(faceInformation);
 
-
-
-
-}
+        }).catch(function () {
+            alert("Could not retrieve the information. Please try again later.")
+        });
 
 
-const requestFace = function (image,cb) {
-
-    const apiKey = "z3RiaROksCQrhNWjl9AanKkCbEDUtV5W";
-    const apiSecret = "rF3gmv1TiXk2Mco19sYSMvcl71lCfUfm";
-    const faceURL = `https://api-us.faceplusplus.com/facepp/v3/detect?api_key=${apiKey}&api_secret=${apiSecret}&image_url=${image}`;
 
 
-    $.ajax({
-        url: faceURL,
-        method: 'POST',
-    }).then(function (response) {
+    }
 
-        const face = response.faces;
 
-        analyzeFace(apiKey, apiSecret, face[0].face_token, face, cb);
-        
-    }).catch(function () {
-        alert("Could not retrieve the information. Please try again later.")
-    });
+    const requestFace = function (image, cb) {
 
-}
+        const apiKey = "z3RiaROksCQrhNWjl9AanKkCbEDUtV5W";
+        const apiSecret = "rF3gmv1TiXk2Mco19sYSMvcl71lCfUfm";
+        const faceURL = `https://api-us.faceplusplus.com/facepp/v3/detect?api_key=${apiKey}&api_secret=${apiSecret}&image_url=${image}`;
 
-const clipFace = function(information){
 
-    const width = information.width;
-    const height = information.height;
+        $.ajax({
+            url: faceURL,
+            method: 'POST',
+        }).then(function (response) {
 
-    // url = cloudinary.image("40648536_10156056664519285_7445547226065010688_n.jpg", {gravity: "face", height: height, width: width, crop: "fill"})
+            const face = response.faces;
 
-    // url = `https://res.cloudinary.com/dyais46lc/image/upload/c_fill,g_face,h_${height},w_${width}/v1539307306/40648536_10156056664519285_7445547226065010688_n.jpg`;
-    url = `https://res.cloudinary.com/dyais46lc/image/upload/c_crop,g_face,h_${height},w_${width}/v1539310622/faceSquare.jpg`;
+            analyzeFace(apiKey, apiSecret, face[0].face_token, face, cb);
 
-    const pictureDiv = $("<div>").addClass("makiko-test");
+        }).catch(function () {
+            alert("Could not retrieve the information. Please try again later.")
+        });
 
-    // const faceImg = $("img").attr("src", url);
-    // faceImg.attr("id", "mv-test");
+    }
 
-    const faceImg = $("<img>");
-    faceImg.attr("src", url);
-    faceImg.attr("id", "mv-test");
-    faceImg.addClass("img-fluid");
+    const clipFace = function (information, pictureName) {
 
-    pictureDiv.append(faceImg);
+        const width = information.width;
+        const height = information.height;
 
-    $("#makiko").append(pictureDiv);
 
-}
+        // url = cloudinary.image("40648536_10156056664519285_7445547226065010688_n.jpg", {gravity: "face", height: height, width: width, crop: "fill"})
+
+        // url = `https://res.cloudinary.com/dyais46lc/image/upload/c_fill,g_face,h_${height},w_${width}/v1539307306/40648536_10156056664519285_7445547226065010688_n.jpg`;
+        url = `https://res.cloudinary.com/dyais46lc/image/upload/c_crop,g_face,h_${height},w_${width}/${pictureName}`;
+
+        const pictureDiv = $("<div>").addClass("makiko-test");
+
+        // const faceImg = $("img").attr("src", url);
+        // faceImg.attr("id", "mv-test");
+
+        const faceImg = $("<img>");
+        faceImg.attr("src", url);
+        faceImg.attr("id", "mv-test");
+        faceImg.addClass("img-fluid");
+
+        pictureDiv.append(faceImg);
+
+        $("#statusWindow").append(pictureDiv);
+
+    }
+
+// });
