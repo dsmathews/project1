71fd57fcff7a766cd4d4fcc93315b0e3f30da044
Darrell Mathews <smathewsbd@gmail.com>
2018-10-14 12:40:14 -0400

    update to universal passport senction

---
+++ speciespassport.js
@@ -0,0 +1,46 @@
+const Passport = function() {
+    $('#passportCard').empty();
+    const command = '';
+    const mask = '';
+
+    const passCard = $('<div>');
+    passCard.addclass('card-body');
+
+    // const pirate = function() {
+    //     let command = pirate;
+    //     let mask = pirate;
+    //     console.log('This is the day you will always remember as the day you almost caught Captain Jack Sparrow');
+    
+    // };
+
+    // const vulcan = function() {
+    //     let command =vulcan;
+    //     console.log('fascinating');
+    // };
+
+    // const klingon = function () {
+    //     console.log('Qapla');
+    // }
+
+    // const romulan = function () {
+    //     let command = romulan;
+    //     console.log('Beware Romulans bearing gifts');
+    // }
+
+    // const elves = function () {
+    //     let command = elves;
+    //     console.log('lothlorien');
+    // };
+
+    // const swedChef = function () {
+    //     let command = swedChef;
+    //     console.log('hurdy gurdy um di chicky');
+    // };
+
+    
+
+
+
+   
+    $('#passportCard').append();
+};
