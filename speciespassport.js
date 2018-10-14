bc3d729c9be5beb742d123aafc6f24c69bbf10d1
Darrell Mathews <smathewsbd@gmail.com>
2018-10-14 17:07:42 -0400

    test

--- speciespassport.js
+++ speciespassport.js
@@ -6,62 +6,68 @@ const Passport = function() {
     const passCard = $('<div>');
     passCard.addclass('card-body');
 
-    // const pirate = function() {
-    //     let command = pirate;
-    //     let mask = pirate;
-    //     console.log('This is the day you will always remember as the day you almost caught Captain Jack Sparrow');
+    const yoda = function () {
+        let command = yoda;
+        let mask = yoda;
+        console.log('do or do not, there is not try');
+    }
     
-    // };
+    const pirate = function() {
+        let command = pirate;
+        let mask = pirate;
+        console.log('This is the day you will always remember as the day you almost caught Captain Jack Sparrow');
+    
+    };
 
-    // const vulcan = function() {
-    //     let command =vulcan;
-    //     console.log('fascinating');
-    // };
+    const vulcan = function() {
+        let command =vulcan;
+        console.log('fascinating');
+    };
 
-    // const klingon = function () {
-    //     console.log('Qapla');
-    // }
+    const klingon = function () {
+        console.log('Qapla');
+    };
 
-    // const romulan = function () {
-    //     let command = romulan;
-    //     console.log('Beware Romulans bearing gifts');
-    // }
+    const romulan = function () {
+        let command = romulan;
+        console.log('Beware Romulans bearing gifts');
+    };
 
-    // const elves = function () {
-    //     let command = elves;
-    //     console.log('lothlorien');
-    // };
+    const elves = function () {
+        let command = elves;
+        console.log('lothlorien');
+    };
 
-    // const swedChef = function () {
-    //     let command = swedChef;
-    //     console.log('hurdy gurdy um di chicky');
-    // };
+    const swedChef = function () {
+        let command = swedChef;
+        console.log('hurdy gurdy um di chicky');
+    };
 
     
-    // const flowDirect = function() {
-    //     event.preventDefault();
-    //     switch (command) {
-    //         case 'pirate':
-    //             pirate ();
-    //             break;
-    //         case 'vulcan':
-    //             vulcan();
-    //             break;
-    //         case 'klingon':
-    //             klingon();
-    //             break;
-    //         case 'romulan':
-    //             romluan();
-    //             break;
-    //         case 'elves':
-    //             elves();
-    //             break;
-    //         case 'swedChef':
-    //             swedChef();
-    //             break;
-    //     }
-    //     console.log("switch");
-    // };
+    const flowDirect = function() {
+        event.preventDefault();
+        switch (command) {
+            case 'pirate':
+                pirate ();
+                break;
+            case 'vulcan':
+                vulcan();
+                break;
+            case 'klingon':
+                klingon();
+                break;
+            case 'romulan':
+                romluan();
+                break;
+            case 'elves':
+                elves();
+                break;
+            case 'swedChef':
+                swedChef();
+                break;
+        }
+        console.log("switch");
+    };
 
 
 
