7588dda45c6442a89a2cbb3be3757a4a28fa5c7d
Darrell Mathews <smathewsbd@gmail.com>
2018-10-15 11:28:10 -0400

    updated speciesPassport.js

--- speciespassport.js
+++ speciespassport.js
@@ -1,139 +1,117 @@
-const Passport = function() {
+const speciesPassport = function() {
     $('#passportCard').empty();
-    const command = '';
+    let command = alienChosen;
 
     // build area
     const passCard = $('<div>');
-    passCard.addclass('card-body');
+    passCard.addClass('card-body');
 
-    const home ='';
-    const species = '';
-    const businessType = '';
-    const alertStatus = '';
-
-    
-    home.addclass('<p>');
-    species.addclass('<p>');
-    businessType.addclass('<p>');
-    alertStatus.addclass('<p>');
-
-
-
+    let home ='';
+    let species = '';
+    let businessType = '';
+    let alertStatus = '';
 
 
     const yoda = function () {
-        let command = 'yoda';
-        let home = 'Dagobah';
-        let species = 'unknown';
-        let businessType = 'republic business';
-        let alertStatus = 'allow to proceed';
+        home = 'Dagobah';
+        species = 'unknown';
+        businessType = 'republic business';
+        alertStatus = 'allow to proceed';
         
         console.log('do or do not, there is not try');
     };
     
     const pirate = function() {
-        let command = 'pirate';
-        let home = 'Tortuga';
-        let species = 'scallywag';
-        let businessType = 'marauder';
-        let alertStatus = 'observe with caution';
+        home = 'Tortuga';
+        species = 'scallywag';
+        businessType = 'marauder';
+        alertStatus = 'observe with caution';
         console.log('This is the day you will always remember as the day you almost caught Captain Jack Sparrow');
     
     };
 
     const vulcan = function() {
-        let command ='vulcan';
-        let home = 'Vulcan';
-        let species = 'Vulcan';
-        let businessType = 'VSA Observation';
-        let alertStatus = 'Live long and prosper';
+        home = 'Vulcan';
+        species = 'Vulcan';
+        businessType = 'VSA Observation';
+        alertStatus = 'Live long and prosper';
 
         console.log('fascinating');
     };
 
     const klingon = function () {
-        let command = 'klingon';
-        let home = 'Qonos';
-        let species = 'Klingon';
-        let businessType = 'Diplomatic Envoy with Starfleet';
-        let alertStatus = 'observe with caution';
+        home = 'Qonos';
+        species = 'Klingon';
+        businessType = 'Diplomatic Envoy with Starfleet';
+        alertStatus = 'observe with caution';
         console.log('Qapla');
     };
 
     const romulan = function () {
-        let command = 'romulan';
-        let home = 'Romulus';
-        let species = 'Romulan';
-        let businessType = 'Meeting with Federation High Council';
-        let alertStatus = 'observe with caution';
+        home = 'Romulus';
+        species = 'Romulan';
+        businessType = 'Meeting with Federation High Council';
+        alertStatus = 'observe with caution';
         console.log('Beware Romulans bearing gifts');
     };
 
     const sindarin = function () {
-        let command = 'elves';
-        let home = 'Lothlorien';
-        let species = 'Elves';
-        let businessType = 'Meeting with Gandalf the Grey';
-        let alertStatus = 'allow to proceed';
+        home = 'Lothlorien';
+        species = 'Elves';
+        businessType = 'Meeting with Gandalf the Grey';
+        alertStatus = 'allow to proceed';
         console.log('lothlorien');
     };
 
-    const swedChef = function () {
-        let command = 'swedChef';
-        let home = 'Jim Hensons Creature Shop';
-        let species = 'Muppet';
-        let businessType = 'Cooking Convention';
-        let alertStatus = 'Bork Bork Bork';
+    const chef = function () {
+        home = 'Jim Hensons Creature Shop';
+        species = 'Muppet';
+        businessType = 'Cooking Convention';
+        alertStatus = 'Bork Bork Bork';
         console.log('hurdy gurdy um di chicky');
     };
 
-    const elmer = function () {
-        let command = 'fudd';
-        let home = 'Warner Studios';
-        let species = 'Human';
-        let businessType = 'Huntin Wabbits';
-        let alertStatus = 'observe with caution';
+    const fudd = function () {
+        home = 'Warner Studios';
+        species = 'Human';
+        businessType = 'Huntin Wabbits';
+        alertStatus = 'observe with caution';
         console.log('wabbit season');
     };
 
-    const william = function() {
-        let command = 'shakespeare';
-        let home = 'Stratford upon Avon';
-        let species = 'Human';
-        let businessType = 'Globe Theater reconstruction';
-        let alertStatus = 'no harm shall come to thee until Burnam Wood comes to Dunsinane';
+    const shakespeare = function() {
+        home = 'Stratford upon Avon';
+        species = 'Human';
+        businessType = 'Globe Theater reconstruction';
+        alertStatus = 'no harm shall come to thee until Burnam Wood comes to Dunsinane';
         console.log('Beware the ides of March');
     };
 
-    const darth = function() {
-        let command = 'sith';
-        let home = 'Tortuga';
-        let species = 'scallywag';
-        let businessType = 'marauder';
-        let alertStatus = 'observe with Extreme caution';
+    const sith = function() {
+        home = 'Tortuga';
+        species = 'scallywag';
+        businessType = 'marauder';
+        alertStatus = 'observe with Extreme caution';
         console.log('always two there are');
     };
 
     const gungan = function() {
-        let command = 'jarjar';
-        let home = 'Naboo';
-        let species = 'Gungan';
-        let businessType = 'Republic Senate';
-        let alertStatus = 'allow to proceed';
+        home = 'Naboo';
+        species = 'Gungan';
+        businessType = 'Republic Senate';
+        alertStatus = 'allow to proceed';
         console.log('meesa no worry');
     };
 
-    const hutt = function() {
-        let command = 'jabba';
-        let home = 'Nal-Hutta';
-        let species = 'Hutt';
-        let businessType = 'legitimate business transaction';
-        let alertStatus = 'observe with Extreme caution';
+    const huttese = function() {
+        home = 'Nal-Hutta';
+        species = 'Hutt';
+        businessType = 'legitimate business transaction';
+        alertStatus = 'observe with Extreme caution';
         console.log('tell jabba I will bring him the money');
     };
     
     const flowDirect = function() {
-        event.preventDefault();
         switch (command) {
             case 'pirate':
                 pirate ();
@@ -147,34 +125,34 @@ const Passport = function() {
             case 'romulan':
                 romluan();
                 break;
-            case 'elves':
-                elves();
+            case 'sindarin':
+                sindarin();
                 break;
-            case 'swedChef':
-                swedChef();
+            case 'chef':
+                chef();
                 break;
             case 'fudd':
-                elmer();
+                fudd();
                 break;
             case 'shakespeare':
-                william();
+                shakespeare();
                 break;
             case 'sith':
-                darth();
+                sith();
                 break;
-            case 'jarjar':
+            case 'gungan':
                 gungan();
                 break;
-            case 'jabba':
-                hutt();
+            case 'huttese':
+                huttese();
                 break;
+        
         }
         console.log("switch");
     };
+    flowDirect();
 
-
-
-   
-    $('#passportCard').append();
+    passCard.html(`Home: ${home}<br /> Species: ${species}<br /> Business: ${businessType}<br />Alert Status: ${alertStatus}`);
+    $('#passportCard').append(passCard);
 };
 
